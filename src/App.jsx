f2eafe897850c137dc2acdf31bfe3a221440e7c6
OUA-FAA <ouafaa.chafiq@emsi-edu.ma>
2025-12-22 16:24:30 +0100

    TD01 : Ajouter Header avec logo, Content et Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import emsi from './assets/emsi.png'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Header() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Ouafaa + MBDS</h1>
+    <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+      <img 
+        src={emsi} 
+        alt="Logo Formation" 
+        style={{ width: '200px', height: 'auto' }}
+      />
+      <h1>Introduction à React</h1>
+      <h2>A la découverte des premières notions de React</h2>
+    </div>
+  )
+}
+
+
+
+  function Content() {
+   const [count, setCount] = useState(0)
+
+   return (
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,6 +33,16 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      ) 
+      }
+
+      function App() {
+       return ( 
+       <> 
+       <Header />
+
+  
+      <Content />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
