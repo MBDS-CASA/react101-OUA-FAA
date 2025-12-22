2b51d897a914233a6d6b3360ffe443491f12e24f
OUA-FAA <ouafaa.chafiq@emsi-edu.ma>
2025-12-22 17:00:31 +0100

    TD02: Afficher la date et l'heure du jour

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,18 @@ function Header() {
 }
 
 function MainContent() {
+  const now = new Date();
+
+  const jour = now.getDate();
+  const mois = now.getMonth() + 1;
+  const annee = now.getFullYear();
+  const heure = now.getHours();
+  const minute = now.getMinutes();
+  const seconde = now.getSeconds();
+
   return (
     <main style={{ textAlign: 'center', marginBottom: '2rem' }}>
-      Ici, nous afficherons des informations interessantes :)
+      Bonjour, on est le {jour}/{mois}/{annee} et il est {heure}:{minute}:{seconde}
     </main>
   )
 }
